80d9752af9057ce238318e027dac374081340427
iamshakur <salmanshakur7@gmail.com>
2023-11-08 14:06:40 +0100

    animation

--- index.js
+++ index.js
@@ -10,3 +10,47 @@ menu.forEach((element) => {
 		icon.classList.toggle("active");
 	});
 });
+//Animation on scroll........................
+ScrollReveal({ distance: "60px", duration: 2000, delay: 400 });
+
+ScrollReveal().reveal(".hero-img, .hero", {
+	delay: 110,
+	origin: "right",
+	// interval: 500,
+	duration: 2200,
+	distance: "50px",
+});
+
+ScrollReveal().reveal(".about-textbox, .about, .service, .why", {
+	delay: 110,
+	origin: "bottom",
+	interval: 500,
+	duration: 2200,
+});
+
+ScrollReveal().reveal(".other, .own", {
+	delay: 110,
+	origin: "right",
+	interval: 600,
+	duration: 2200,
+});
+
+ScrollReveal().reveal(".work-img", {
+	delay: 110,
+	origin: "left",
+	interval: 400,
+	duration: 2200,
+});
+ScrollReveal().reveal(".work-box", {
+	delay: 110,
+	origin: "right",
+	interval: 600,
+	duration: 2200,
+});
+
+ScrollReveal().reveal(".work", {
+	delay: 110,
+	origin: "buttom",
+	interval: 600,
+	duration: 2200,
+});
